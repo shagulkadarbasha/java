f388721a6f5a6f7bb02aa9ff3729ffe92e38d25c
shagulkadarbasha <shagulkmech@gmail.com>
2022-01-10 15:19:51 +0530

    done

---
+++ src/org/git/Employee.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class Employee {
+	public void add() {
+		System.out.println("addition");
+	
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+
+	}
+
+}
